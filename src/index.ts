70f857facd57ed92466633d4174511f3bf41544e
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-07-07 23:16:26 -0300

    DEL: remove semicolons

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+const app: Express = express()
+const port = process.env.PORT || 3000
 
 const loginRouter = require('./routes/login')
 
 app.get("/serverStatus", (req: Request, res: Response) => {
-  res.send({ status: 'online'} );
-});
+  res.send({ status: 'online'} )
+})
 
 app.use('/login', loginRouter)
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
