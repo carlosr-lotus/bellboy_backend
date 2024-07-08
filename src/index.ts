a4e8e5f6812b5d3bb95b27c20801cc19548a19f8
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-07-07 23:27:07 -0300

    ADD: cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3000
+const corsOptions = {
+  origin: 'http://localhost:4000'
+}
 
-const loginRouter = require('./routes/login')
+app.use(cors(corsOptions))
 
 app.get("/serverStatus", (req: Request, res: Response) => {
   res.send({ status: 'online'} )
 })
+
+const loginRouter = require('./routes/login')
 
 app.use('/login', loginRouter)
 
