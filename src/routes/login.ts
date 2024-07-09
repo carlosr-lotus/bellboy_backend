e615d17c242b52caa07579046d29127c5588bc61
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-07-08 21:32:50 -0300

    UPD: fetch user request

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -5,19 +5,28 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const router = express.Router()
-const db = new Database(process.env.DATABASE, {})
+const db = new Database(process.env.DATABASE, { fileMustExist: true })
 
 router.get('/', (req: Request, res: Response) => {
-    getUsers(res)
+    const {
+        email,
+        password
+    } = req.query
+
+    if (!email) res.status(400).json({ error: 'Email param not provided.' })
+    if (!password) res.status(400).json({ error: 'Password param not provided.' })
+
+    getUser(email as string, password as string, res)
 })
 
-function getUsers(res: Response) {
+function getUser(email: string, password: string, res: Response) {
     try {
-        const query = "SELECT * FROM t_Users"
+        const query = "SELECT * FROM t_Users_Login WHERE email = ? and password = ?"
 
-        const data = db.prepare(query).all()
+        const stmt = db.prepare(query)
+        const row = stmt.get(email, password)
 
-        res.status(200).json(data)
+        res.status(200).json(row)
     } catch(err) {
         res.status(500).json(err)
     }
